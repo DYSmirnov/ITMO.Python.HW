4cb2a7272927e348463e9d1816ccae335dd95436
Администратор <@WIN-GMAKK9MSC73>
2022-06-10 08:57:35 +0300

    lab5

---
+++ Lab5/name.py
@@ -0,0 +1,4 @@
+def enter_name(number):
+    player_name = input(f'Введите имя {number}-го играющего: ')
+    print(number)
+    return player_name
---
+++ Lab5/programma.py
@@ -0,0 +1,4 @@
+import name
+
+player1 = name.enter_name(1)
+player2 = name.enter_name(2)
